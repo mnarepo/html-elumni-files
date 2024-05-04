f49bf855ad62d0056be89748ba9a566d2e5831f0
mnapro <nabeelar069@gmail.com>
2024-05-04 17:32:22 +0500

    04-May-24 CW

---
+++ 03-May-CW/index.js
@@ -0,0 +1,16 @@
+// var a = 10;
+// console.log(a);
+
+// let a = 10;
+// console.log(a);
+
+// const a = 10;
+// console.log(a);
+
+// let a = 10;
+// let b = 5;
+// b+=a;
+// console.log(b);
+
+// a>b ? true:false; //Conditional statement
+
