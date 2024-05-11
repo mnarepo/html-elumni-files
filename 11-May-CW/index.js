5131fcde5e4881de3a4030b9a7ab35aa72d29bea
mnapro <nabeelar069@gmail.com>
2024-05-11 17:53:08 +0500

    11-May-24 CW

---
+++ 11-May-CW/index.js
@@ -0,0 +1,33 @@
+// Task 01
+
+// var clothes = [];
+// clothes.push("shirt");
+// clothes.push("pent");
+// clothes.push("tie");
+// clothes.push("belt");
+// clothes.push("trouser");
+// clothes.pop(clothes[4]);
+// clothes.push("t-shirt");
+// console.log(clothes);
+
+// Task 02
+
+// var favCar = [];
+// favCar.color = "black";
+
+
+
+// Class Work
+var car = {};
+car.mileage = 98765;
+car.color = "red";
+console.log(car);
+car.turnTheKey = function(){
+    console.log("The Engine is RUnning");
+}
+car.lightsOn = function(){
+    console.log("The Lights are on");
+}
+console.log(car);
+car.turnTheKey();
+car.lightsOn();
